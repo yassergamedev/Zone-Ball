b905b7817e9d038be0c572fe4ac3fa41d4680c21
yassergamedev <114354646+yassergamedev@users.noreply.github.com>
2024-02-23 16:18:47 +0100

    Added Ball Possession Logic

---
+++ Assets/Scripts/Players/PlayerManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    void Start()
+    {
+        CheckPossession();
+    }
+
+    void CheckPossession()
+    {
+        GameObject ball = GameObject.FindGameObjectWithTag("Ball");
+
+        // Check if the ball is in possession
+        if (ball != null && ball.transform.IsChildOf(transform))
+        {
+             Debug.Log("I am " + gameObject.name + " from team "+ gameObject.transform.parent + " and i have possession of the ball");
+            
+        }
+        else
+        {
+           Debug.Log("I am " + gameObject.name + " from team "+ gameObject.transform.parent + 
+           "and i know that " +
+           ball.transform.parent + " from team " + ball.transform.parent.transform.parent.name + " is in possession of the ball");
+        }
+    }
+}
