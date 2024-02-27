387924c7ce3e0e4ac74109ebd8c217431d8cf753
yassergamedev <114354646+yassergamedev@users.noreply.github.com>
2024-02-27 10:00:07 +0100

    PlayerMovement init

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -1,18 +1,100 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public PossessionManager possessionManager;
+    string playerTag = gameObject.tag;
+    bool isInOwnHalf = false;
+    //true == left
+    //false == right
+    private float movementDirection = 0f;
+    string Half ;
+
+
     void Start()
     {
+        if(playerTag == "Player")
+        {
+            Half = "RightHalf";
+          
+        }
+        else{
+            Half = "LeftHalf";
         
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private string CheckPlay()
     {
         
+                // Call CheckPossession function from PossessionManager
+                GameObject possession = possessionManager.CheckPossession();
+                
+                // If possession is null, return "Defense"
+                if (possession == null)
+                {
+                    return "Defense";
+                }
+                // If possession is not null, return "Offense"
+                else
+                {
+                    return "Offense";
+                }
+        // Return an empty string if the gameObject's tag is not Player or OppPlayer
+        return "";
     }
+
+    private void MovePlayer()
+    {
+        // Check the play mode (Offense or Defense)
+        string playMode = CheckPlay();
+
+       
+    if(playMode == "Offense")
+    {
+
+    
+        // If the player is in his own half
+        if (isInOwnHalf)
+        {
+            // Generate a random number between 0 and 99
+            int randomNumber = Random.Range(0, 100);
+
+            // If the random number is less than or equal to 90 (90% chance)
+            if (randomNumber <= 90)
+            {
+                // Move the player to the opposite direction (left for Offense, right for Defense)
+                transform.Translate(Vector3.left * Time.deltaTime);
+            }
+            // Otherwise (10% chance)
+            else
+            {
+                // Move the player randomly
+                float randomX = Random.Range(-1f, 1f);
+                float randomY = Random.Range(-1f, 1f);
+                Vector3 randomDirection = new Vector3(randomX, randomY, 0f);
+                transform.Translate(randomDirection * Time.deltaTime);
+            }
+        }
+        
+        // If the player is not in his own half
+        else
+        {
+            // Move the player to the opposite direction (right for Offense, left for Defense)
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+    }
+    }
+
+     void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.compareTag(Half))
+        {
+            isInOwnHalf = true;
+        }
+        else{
+            isInOwnHalf = false;
+        }
+    }
+
 }
