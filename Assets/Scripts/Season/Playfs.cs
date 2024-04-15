5f87766ab63226bb42bf57b40e2be720a69964c9
yassergamedev <114354646+yassergamedev@users.noreply.github.com>
2024-04-15 22:50:44 +0100

    changed some shit

---
+++ Assets/Scripts/Season/Playfs.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playfs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Round16;
+    public GameObject[] Quarters;
+    public GameObject[] Semis;
+  public GameObject[] Final;
+   
+    // Update is called once per frame
+   public void setGames(string[] r16, string[] quarters, string[] semis, string[]final)
+    {
+        for(int i = 0; i < r16.Length;i++)
+        {
+
+        }
+    }
+}
